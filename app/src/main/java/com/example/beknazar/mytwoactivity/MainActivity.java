fba15f4cd85f1d4572a64bde7f650850cfdcc82e
Beknazar <beknazark@inbox.ru>
2018-06-21 12:27:52 +0600

    "Add two Activity"

--- app/src/main/java/com/example/beknazar/mytwoactivity/MainActivity.java
+++ app/src/main/java/com/example/beknazar/mytwoactivity/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.beknazar.mytwoactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+
+ Button btnActTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnActTwo=(Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnActTwo:
+                break;
+            default:
+                break;
+
+        }
     }
 }
